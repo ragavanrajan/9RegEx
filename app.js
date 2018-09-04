9953ec8b1c150311a5aa16b1fb2387b17efc3a2f
Ragavan Rajan <rr100@students.waikato.ac.nz>
2018-09-05 10:03:49 +1200

    Initial commit

---
+++ app.js
@@ -0,0 +1,45 @@
+let re;
+// this is the pattern
+re = /hello/;
+
+re = /hello/i; // i = case insensitive 
+// re = /hello/g; // Global search
+
+
+// console.log(re);
+// console.log(re.source);
+
+
+//exec() - Return result in an array or null
+
+/* const result = re.exec('hello world');
+
+console.log(result);
+console.log(result[0]);
+console.log(result.index);
+console.log(result.input);
+ */
+
+//  test() - Returns true or false
+/* 
+const result = re.test('Hello');
+// case sensitive match
+console.log(result); */
+
+// match() - Return rsult array or null 
+
+/* const str = 'Hello There';
+const result = str.match(re);
+console.log(result); */
+
+//search() - Returns the index of the first match if not found returns  -1 
+
+/* const str = 'Hello There';
+const result = str.search(re);
+console.log(result); */
+
+// replace() - Return new string with some or all matches of a pattern
+
+const str = 'Hello There';
+const newStr = str.replace(re, 'Hi');
+console.log(newStr);
